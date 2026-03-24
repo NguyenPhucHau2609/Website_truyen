c0fc6e2a6edeebb74676837a7129de3e12ed362d
Trịnh Công Sơn <chatgpt03012004@gmail.com>
2026-03-24 23:00:58 +0700

    fix: login

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -31,11 +31,6 @@ api.interceptors.response.use(
 );
 
 // Auth
-export const login = (username, password) => api.post('/auth/signin', { username, password });
-export const register = (username, email, password) => api.post('/auth/signup', { username, email, password });
-
-
-// Auth
 export const login = (username, password) =>
   api.post("/auth/signin", { username, password });
 export const register = (username, email, password) =>
